30b1e80f490fcd096fb4fcf1b3e31c6f5cd89382
AncientDom <96964678+AncientDom@users.noreply.github.com>
2026-01-04 16:07:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'package:printing/printing.dart';
 // ==========================================
 
 // TODO: GET YOUR KEY HERE: https://aistudio.google.com/app/apikey
-const String _apiKey = "AIzaSyBfyXr67_f6C_WTDrPIoMYEbCBrJB08mLg"; 
+const String _apiKey = "AIzaSyCvIGSWr1xqh6t7GI5gmEhBnZ_E3XJBSV4"; 
 
 void main() {
   runApp(const MyApp());
